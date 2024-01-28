d387f60067e8946f8a938247db157182d14822b8
ezz456CH <147528579+ezz456CH@users.noreply.github.com>
2024-01-28 08:48:37 +0700

    Update

--- html/script.js
+++ html/script.js
@@ -5228,7 +5228,7 @@ function setGlobalScale(scale, init) {
     globalScale = scale;
     document.documentElement.style.setProperty("--SCALE", globalScale);
 
-    labelFont = "bold " + (12 * globalScale * labelScale) + "px/" + (14 * globalScale * labelScale) + "px Tahoma, Verdana, Helvetica, sans-serif";
+    labelFont = (12 * globalScale * labelScale) + "px/" + (14 * globalScale * labelScale) + "CascadiaCode, sans-serif";
 
     checkScale();
     setLineWidth();
