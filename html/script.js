6d2d94564cefae39d634917224d4b90ef5283805
ezz456CH <147528579+ezz456CH@users.noreply.github.com>
2024-03-09 16:00:27 +0700

    test with RefreshInterval

--- html/script.js
+++ html/script.js
@@ -1884,7 +1884,7 @@ function setIntervalTimers() {
     }
     if (enable_pf_data && !pTracks && !globeIndex) {
         jQuery('#pf_info_container').removeClass('hidden');
-        timers.pf_data = window.setInterval(fetchPfData, RefreshInterval*10.314);
+        timers.pf_data = window.setInterval(fetchPfData, RefreshInterval);
         fetchPfData();
     }
     if (receiverJson && receiverJson.outlineJson) {
