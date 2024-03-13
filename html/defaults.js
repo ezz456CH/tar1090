562dffd2dbea190dde7138e1982409cbdc93ec6a
ezz456CH <147528579+ezz456CH@users.noreply.github.com>
2024-03-13 18:24:19 +0700

    update index, style.css, title and icon

--- html/defaults.js
+++ html/defaults.js
@@ -196,7 +196,7 @@ let SiteCirclesColors = ['#000000', '#000000', '#000000'];
 let SiteCirclesLineDash = null; // null - solid line, [5, 5] - dashed line with 5 pixel lines and spaces in between
 
 // Controls page title, righthand pane when nothing is selected
-let PageName = "adsb.ezz456ch.xyz";
+let PageName = "ADS-B - ezz456CH";
 
 // Show country flags by ICAO addresses?
 let ShowFlags = true;
