12b4ad8feab24663cf8ae812a3527f4946228ab7
ezz456CH <ezz456chgmok@gmail.com>
2024-04-22 00:55:47 +0700

    update font size again

--- html/script.js
+++ html/script.js
@@ -6514,7 +6514,7 @@ function createLocationDot() {
     let markerStyle = new ol.style.Style({
         text: new ol.style.Text({
             text: '+',
-            font: ((20 * globalScale) + 'px Cascadia Code'),
+            font: ((14 * globalScale) + 'px Cascadia Code'),
             fill: new ol.style.Fill({ color: '#000000' }),
         })
     });
