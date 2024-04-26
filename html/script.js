92d0f79f1885fcc72f1e678f9ba80cb441acf8e3
ezz456CH <ezz456chgmok@gmail.com>
2024-04-26 16:57:41 +0700

    update delta again

--- html/script.js
+++ html/script.js
@@ -2173,7 +2173,7 @@ function ol_map_init() {
         interactions: ol.interaction.defaults({altShiftDragRotate:false, pinchRotate:false}).extend([
             new ol.interaction.DblClickDragZoom({
                 duration: 0,
-                delta: 0.05
+                delta: 0.001
             })
         ]),
     });
