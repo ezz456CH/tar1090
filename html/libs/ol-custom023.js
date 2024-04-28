460140f3c05019ab6e2c5bd49eacf3cde7fb6c6b
ezz456CH <ezz456chgmok@gmail.com>
2024-04-28 11:37:42 +0700

    inverted DblClickDragZoom

--- html/libs/ol-custom023.js
+++ html/libs/ol-custom023.js
@@ -20723,7 +20723,7 @@
 			let e = 1;
 			const i = this.targetPointers[0],
 				n = this.down_.originalEvent,
-				r = i.clientY - n.clientY;
+				r = -(i.clientY - n.clientY);
 			void 0 !== this.lastDistance_ && (e = 1 - (this.lastDistance_ - r) * this.scaleDeltaByPixel_), this.lastDistance_ = r, 1 != e && (this.lastScaleDelta_ = e);
 			const s = t.map,
 				o = s.getView();
