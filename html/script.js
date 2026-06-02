9af4d92e042f7c773f6e41a714c6e86e9f2c0ef0
ezz456CH <earth@ezz456ch.com>
2026-06-02 11:33:28 +0700

    Merge remote-tracking branch 'upstream'

--- html/script.js
+++ html/script.js
@@ -2490,7 +2490,6 @@ function startPage() {
 
     if (replay) {
         showReplayBar();
-        loadReplay(replay.ts);
     }
 
     if (heatmap) {
@@ -3088,6 +3087,9 @@ function initMap() {
     if ((globeIndex && aggregator) || filterUuid) {
         jQuery('#dump1090_message_rate_td').hide();
     }
+    if ((globeIndex && aggregator) || (receiverJson && receiverJson.haveReplay)) {
+        jQuery('#RP').show();
+    }
 
     locationDotLayer = new ol.layer.Vector({
         name: "locationDot",
@@ -3434,16 +3436,19 @@ function initMap() {
                     toggleFollow();
                     break;
                 // filters
-                case "T":
-                    filterTISB = !filterTISB;
-                    refreshFilter();
-                    break;
-                case "u":
-                    toggleMilitary();
-                    break;
-                case "i":
-                    toggleIsolation();
-                    break;
+            case "T":
+                filterTISB = !filterTISB;
+                refreshFilter();
+                break;
+            case "Y":
+                showReplayBar();
+                break;
+            case "u":
+                toggleMilitary();
+                break;
+            case "i":
+                toggleIsolation();
+                break;
                 // persistence mode
                 case "p":
                     togglePersistence();
@@ -7379,7 +7384,7 @@ function updateAddressBar() {
     }
     //console.log(shareLink);
 
-    if (!string && !usp.has("showTrace") && !usp.has("icao")) {
+    if (!string && !usp.has('showTrace') && !usp.has('icao') && !usp.has('replay')) {
         string = initialURL;
     } else {
         string = pathName + string;
@@ -9363,20 +9368,12 @@ function showReplayBar() {
     showingReplayBar = !showingReplayBar;
     if (!showingReplayBar) {
         jQuery("#replayBar").hide();
-        if (replay.playing) {
-            replay.playing = false;
-        }
-        replayClear();
+        clearTimeout(refreshId);
         replay = null;
         jQuery("#map_canvas").height("100%");
         jQuery("#sidebar_canvas").height("100%");
         jQuery("#selected_showTrace_hide").show();
-        window.history.replaceState(
-            {},
-            document.title,
-            window.location.pathname
-        );
-        location.reload();
+        fetchData({force: true});
     } else {
         inhibitFetch = true;
 
@@ -9410,7 +9407,6 @@ function showReplayBar() {
         jQuery("#sidebar_canvas").height("calc(100% - 110px)");
         if (!replay) {
             replay = replayDefaults(new Date());
-            replay.playing = false;
         }
         //ts.setUTCMinutes((parseInt((ts.getUTCMinutes() + 7.5)/15) * 15) % 60);
 
@@ -9486,14 +9482,11 @@ function showReplayBar() {
 
         jQuery("#selected_showTrace_hide").hide();
 
-        replayJump();
-
-        if (!replay || replay.playing) {
-            return;
-        }
-        replay.playing = true;
+        loadReplay(replay.ts);
     }
-}
+
+    updateAddressBar();
+};
 
 function timeoutFetch() {
     console.log("timeoutFetch " + localTime(new Date()));
@@ -9728,11 +9721,20 @@ function setAutoselect() {
     autoSelectClosest();
 }
 function registrationLink(plane) {
-    if (plane.country === "Brazil") {
-        return `https://sistemas.anac.gov.br/aeronaves/cons_rab_resposta_en.asp?textMarca=${plane.registration}`;
-    } else {
-        return "";
-    }
+    
+    const countryLinks = {
+        Brazil: (reg) => `https://sistemas.anac.gov.br/aeronaves/cons_rab_resposta_en.asp?textMarca=${reg}`,
+        Australia: (reg) => `https://www.casa.gov.au/search-centre/aircraft-register?reg=${reg.replace(/^VH-/, '')}`,
+        Jamaica: (reg) => `https://www.jcaa.gov.jm/aircraft-registry/${reg}`,
+        Montenegro: (reg) => `https://www.caa.me/en/registri?field_registarska_oznaka1_value=${reg}`,
+        Norway: (reg) => `https://www.luftfartstilsynet.no/aktorer/norges-luftfartoyregister/registrerte-luftfartoy/?mark=${reg}`,
+        Iceland: (reg) => `https://island.is/en/aircraft-registry?aq=${reg.replace(/^TF-/, '')}`,
+        "New Zealand": (reg) => `https://www.aviation.govt.nz/aircraft/aircraft-registration/aircraft-register-search/ShowDetails/${reg.replace(/^ZK-/, '')}`,
+        "United States": (reg) => `https://registry.faa.gov/AircraftInquiry/Search/NNumberResult?nNumberTxt=${reg.slice(1)}`
+    };
+
+    const generator = countryLinks[plane.country];
+    return generator ? generator(plane.registration) : '';
 }
 
 //simple jquery plugin to only update the text when it changes
